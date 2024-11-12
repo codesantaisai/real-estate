c2a522529dc44d1d05bf7844db5e9d2d9cb7b696
Jathurshan Santhirasekaram <124557050+codesantaisai@users.noreply.github.com>
2024-11-12 11:25:52 +0530

    Web3from implemented

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,33 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react'
+import React, { useState } from 'react'
 
 const Contact = () => {
+    const [result, setResult] = useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "68744875-5071-4b94-9c39-220b9ca35015");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      alert("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      alert(data.message);
+      setResult("");
+    }
+  };
     return (
         <div className='text-center p-6 py-20 lg:px-32 w-full overflow-hidden '>
             <h1 className='text-2xl sm:text-4xl font-bold mb-2 text-center'>
@@ -9,7 +35,7 @@ const Contact = () => {
             </h1>
             <p className='text-center text-gray-500 mb-12 max-w-80 mx-auto'>
                 Ready to make a move? Let&apos;s Build Your Future Together</p>
-                <form className='max-w-2xl mx-auto text-gray-600 pt-8' >
+                <form onSubmit={onSubmit} className='max-w-2xl mx-auto text-gray-600 pt-8' >
                     <div className='flex flex-wrap'>
                         <div className='w-full md:w-1/2 text-left'>
                             Your Name
@@ -26,7 +52,7 @@ const Contact = () => {
                         </textarea>
                     </div>
                     <button className="bg-blue-600 text-white py-2 px-12 mb-10 rounded cursor-pointer">
-                        Send Message
+                        {result ? result : "Send Message"}
                     </button>
                 </form>
         </div>
